38dde5aa18fd941c877b14a3e8cda6318c29c444
Vivek Soni <vivek.soni@iitkfirst.com>
2025-10-10 12:22:37 +0530

    fix: Adjust container overflow in App component and update flex properties in OurPride component for better layout

--- src/components/HomePage/OurPride.jsx
+++ src/components/HomePage/OurPride.jsx
@@ -6,12 +6,12 @@ function OurPride() {
     <div className=" mx-auto mt-40 mb-35 ">
       <div className="flex flex-col xl:flex-row items-center justify-between gap-16 xl:gap-8">
         {/* Left side - Carousel */}
-        <div className="flex-1 px-4">
+        <div className="flex-0 px-4">
           <CarouselLayout />
         </div>
 
         {/* Right side - Our Pride */}
-        <div className="flex-shrink-0 md:w-2/3 w-full md:self-end xl:w-[450px]">
+        <div className="flex-1 md:w-2/3 w-full md:self-end xl:1/2 xl:w-[200px]  2xl:w-[450px]">
           <div className="bg-[#F5F5F5]  mx-2 lg:mx-0 md:rounded-l-2xl px-12 py-10 flex flex-col items-start gap-8 shadow">
             <h1 className="text-[42px] font-extralight leading-snug">
               Our{" "}
