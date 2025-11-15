fdb932860e6d27f9e0aa2b5a379ce13465d33124
vivek soni <sonivivek346@gmail.com>
2025-11-15 16:18:32 +0530

    imroved startup card added fallback image for cards and adjust the page layout

--- src/components/Startups/StartupCard.jsx
+++ src/components/Startups/StartupCard.jsx
@@ -1,17 +1,22 @@
 // components/Startups/StartupCard.js
-import React from "react";
+import React, { useState } from "react";
 
 function StartupCard({ data }) {
   // Only wrap in <a> if website is present
+  const [error, setError] = useState(false);
+  const FALL_BACK_IMAGE = "fallback.png";
   const content = (
     <div className="text-white rounded-md group inline-flex w-[260px]  hover:-translate-y-4 transition-all duration-600 ease-in-out flex-col cursor-pointer">
-      <img
-        src={data.image}
-        className="border-1 group-hover:border-transparent border-white"
-        alt={data.title}
-      />
+      <div className="max-w-[260px] max-h-[260px] flex items-center justify-center">
+        <img
+          src={error ? FALL_BACK_IMAGE : data.image}
+          onError={() => setError(true)}
+          className="border-1 group-hover:border-transparent  text-black border-white"
+          alt={data.title}
+        />
+      </div>
       <div className="bg-secondary-blue rounded-b-md flex flex-col gap-1 px-3 py-2 pb-2.5 group-hover:text-black group-hover:bg-white transition-all duration-300 border-2 border-transparent group-hover:border-2 box-border group-hover:border-primary-highlight group-hover:shadow-[0px_3px_3px_0px_rgba(0,0,0,0.20)] ">
-        <h1 className="text-sm font-bold">{data.title}</h1>
+        <h1 className="text-sm font-bold line-clamp-1">{data.title}</h1>
         <p className="text-[10px] line-clamp-1">{data.shortDescription}</p>
       </div>
     </div>
--- src/pages/Startups.jsx
+++ src/pages/Startups.jsx
@@ -4,7 +4,7 @@ import StartupsBanner from "../components/banners/StartupBanner";
 import StartupCard from "../components/Startups/StartupCard";
 import HighlightedText from "./../components/Utility Components/HighlightedText";
 import SearchBar from "./../components/Utility Components/SearchBar";
-import { startupData } from './../data/StartupData';
+import { startupData } from "./../data/StartupData";
 
 function Startups() {
   const [search, setSearch] = useState("");
@@ -18,25 +18,25 @@ function Startups() {
   );
 
   return (
-    <PageLayout
-      bodyStyle={"-mt-60 z-40 p-8 flex flex-col gap-16"}
-      banner={<StartupsBanner />}
-    >
-      <div className="self-end">
-        <SearchBar
-          search={search}
-          setSearch={setSearch}
-          placeholder={"Search for our Startups"}
-        />
-      </div>
-      <h1 className="text-5xl font-thin text-[#1f1f1f] ">
-        <HighlightedText weight={800}>Our</HighlightedText> Startups
-      </h1>
-      <div>
-        <div className="grid lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 gap-6 p-8 xl:justify-items-start justify-items-center grid-cols-1 2xl:grid-cols-5 h-[600px] overflow-y-auto 2xl:gap-8 pt-6 2xl:justify-items-center ">
-          {filteredData.map((startup) => (
-            <StartupCard key={startup.id} data={startup} />
-          ))}
+    <PageLayout bodyStyle={"-mt-60 z-40 p-16  "} banner={<StartupsBanner />}>
+      <div className="flex flex-col gap-10">
+        <div className="flex justify-between">
+          <h1 className="text-5xl font-thin text-[#1f1f1f] ">
+            <HighlightedText weight={800}>Our</HighlightedText> Startups
+          </h1>
+          <SearchBar
+            search={search}
+            setSearch={setSearch}
+            placeholder={"Search for our Startups"}
+          />
+        </div>
+
+        <div>
+          <div className="grid lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 gap-6 p-8 xl:justify-items-start justify-items-center grid-cols-1 2xl:grid-cols-5 h-[600px] overflow-y-auto 2xl:gap-8 pt-6 2xl:justify-items-center ">
+            {filteredData.map((startup) => (
+              <StartupCard key={startup.id} data={startup} />
+            ))}
+          </div>
         </div>
       </div>
     </PageLayout>
